c7a005ffbb38bfa1e36acc94003a3e1e25dd6e48
mogeko-v <64927836+mogeko-v@users.noreply.github.com>
2020-05-23 04:48:25 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function mayorNumero(){
+
+    
+
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+
+    if (num1 >= num2) {
+        if (num1 == num2) {
+            alert("ambos numero son iguales que mogekov perro :v")
+        } else {
+            alert("el numero 1 es mayor que mgekov")
+        }
+
+
+    } else {
+        alert("el numero 2 es mayor que mogekov")
+    }
+
+    
+}
+
+
